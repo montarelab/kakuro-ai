0bb7d9a7f6b374b4d791066cf9e33a51e395be39
dmitriytkachenko350 <dmitriytkachenko350@gmail.com>
2024-11-05 16:52:23 +0100

    modify data structure: replace id for neighbor nodes instead of links, add comments, fix bugs

--- data_structure.py
+++ data_structure.py
@@ -1,6 +1,6 @@
 from entities import Map, Node, NodeList, Clue, Input, Block, RowList, ColList
 from itertools import takewhile
-from dataclasses import dataclass
+from typing import Optional
 import copy
 
 
@@ -15,57 +15,54 @@ def display_matrix(matrix, width=30):
 
 
 
-@dataclass
-class Empty:
+def link_nodes(matrix, cols, rows) -> list[list[Optional[Node]]] :
     '''
-    The class is needed for building a linkage matrix to link all of the nodes
-    '''
-    pass
-
-
-def link_nodes(matrix, cols, rows) -> list[list[Node | Empty]] :
-    '''
-    1. Converts the matrix of elements (Block | Clue | Input) in the matrix of (Node | Empty)
+    1. Converts the matrix of elements (Block | Clue | Input) in the matrix of (Node | None)
     2. Creates links between neighbor Nodes
-    returns: the the matrix of (Node | Empty)
+    returns: the the matrix of (Node | None)
     '''
+
+    id_counter = 0
 
     # get 2d arrray of nodes without links
     node_matrix = copy.deepcopy(matrix)
     for (row_index, row) in enumerate(matrix):
         for (col_index, cell) in enumerate(row):
             if isinstance(cell, Input):
-               node_matrix[row_index][col_index] = Node()
+               node_matrix[row_index][col_index] = Node(id=id_counter, pos_x=col_index, pos_y=row_index)
+               id_counter+=1
             else:
-                node_matrix[row_index][col_index] = Empty()
+                node_matrix[row_index][col_index] = None
 
     # add links to nodes
     for (row_index, row) in enumerate(node_matrix):
         for (col_index, node) in enumerate(row):
-            
+            if node == None:
+                continue
+
             # add upper neighbour
             if row_index != 0:
                 selected_node = node_matrix[row_index - 1][col_index]
-                if isinstance(selected_node, Input):
-                    node.upper_node = selected_node
+                if isinstance(selected_node, Node):
+                    node.upper_node = selected_node.id
             
             # add bottom neighbour
             if row_index != rows - 1:    
                 selected_node = node_matrix[row_index + 1][col_index]
-                if isinstance(selected_node, Input):
-                    node.bottom_node = selected_node
+                if isinstance(selected_node, Node):
+                    node.bottom_node = selected_node.id
 
             # add left neighbour
             if col_index != 0:
                 selected_node = node_matrix[row_index][col_index -1]
-                if isinstance(selected_node, Input):
-                    node.left_node = selected_node
+                if isinstance(selected_node, Node):
+                    node.left_node = selected_node.id
 
             # add right neighbour
             if col_index != cols - 1:
                 selected_node = node_matrix[row_index][col_index + 1]
-                if isinstance(selected_node, Input):
-                    node.right_node = selected_node
+                if isinstance(selected_node, Node):
+                    node.right_node = selected_node.id
 
     # return 2D array of linked Nodes and Empties
     return node_matrix
@@ -107,37 +104,39 @@ def get_data_structure(map: Map) -> tuple [list[Node], list[NodeList]]:
             # create a coll
             if cell.sumCol != None:
                 col_list = ColList(
+                    id = len(all_clues),
                     sum_value = cell.sumCol, 
                     remained_value=cell.sumCol,
                     pos_x=row_index,
                     start_pos_y=col_index,
                     length=0,
-                    list_od_nodes=[])
+                    list_of_nodes=[])
                 
                 # define the length, and get nodes for it
                 array = [row[col_index] for row in node_matrix[row_index + 1:]]
                 nodes = list(takewhile(lambda x: isinstance(x, Node), array))
-                [setattr(node, 'column', col_list) for node in nodes]
+                [setattr(node, 'column', col_list.id) for node in nodes]
                 col_list.length = len(nodes)
-                col_list.list_od_nodes = nodes
+                col_list.list_of_nodes = nodes
                 all_clues.append(col_list)
 
             # create a row
             if cell.sumRow != None:
                 row_list = RowList(
+                    id = len(all_clues),
                     sum_value = cell.sumRow,
                     remained_value=cell.sumCol,
                     pos_y=col_index,
                     start_pos_x=row_index,
                     length=0,
-                    list_od_nodes=[])
+                    list_of_nodes=[])
                 
                 # define the length, and get nodes for it
                 array = node_matrix[row_index][col_index + 1:]
                 nodes = list(takewhile(lambda x: isinstance(x, Node), array))
-                [setattr(node, 'row', row_list) for node in nodes]
+                [setattr(node, 'row', row_list.id) for node in nodes]
                 row_list.length = len(nodes)
-                row_list.list_od_nodes = nodes
+                row_list.list_of_nodes = nodes
                 all_clues.append(row_list)    
 
     return [node for row in node_matrix for node in row if isinstance(node, Node) ], all_clues
