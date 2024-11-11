f48f7147b8ca8151322f83882f064b15d699a104
danil09234 <dailzag05@gmail.com>
2024-11-11 11:29:01 +0100

    Update path to map

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from src.game_controller.controller import KakuroGameController
 from src.ui.kakuro_game_ui import KakuroGameUI
 
-map_path = "maps/map1.json"
+map_path = "maps/9x9_easy.json"
 
 
 def main():
